a9f70c5ce781d3318cdb3bd3328314fbee37d38c
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-04-17 09:16:25 +0530

    Add files via upload

---
+++ InheritancePgm/InheritancePgm/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace InheritPgm
+{
+    public class Employee:Account
+    {
+		private int _salary;
+
+		public int Salary
+		{ 
+			get { return _salary; }
+			set { _salary = value; }
+		}
+		public Employee() { }
+		public Employee(int id,string name,int salary):base(id,name)
+		{
+			Salary = salary;
+		}
+		public void display()
+		{
+			Console.Write($"Salary: {Salary}");
+		}
+	}
+}
---
+++ InheritancePgm/InheritancePgm/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace InheritPgm
+
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            Employee e = new Employee(101,"Anu",10);
+            Console.WriteLine(e);
+        }
+
+    }
+
+    public class Account
+
+    {
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Account() { }
+
+        public Account(int id, string name)
+        {
+            Id= id;
+            Name= name;
+        }
+        public override string ToString()
+        {
+            return $"EmpId: {Id} \t EmpName: {Name} \t Salary: {}";
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
